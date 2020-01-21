eec84e69a57220e3670ef6cbc0ab5dc887345150
bnluong <36832138+bnluong@users.noreply.github.com>
2020-01-21 00:32:04 -0800

    Update MergeSortedArray.java
    Improved the solution of the problem
    Since the two arrays are sorted and nums1 is always going to have enough space, we start at the right most of both arrays (i.e. at m and n)
    If the elements at array 1 is greater than that of nums2 we copy that element to the end nums1 (i.e at m+n)
    Otherwise we copy the element of nums2 to the end of nums1
    Then we iterate until either 1 of them is less than 0 and copy the rest of the other one to nums1

--- MergeSortedArray.java
+++ MergeSortedArray.java
@@ -13,7 +13,7 @@
 	Output: [1,2,2,3,5,6]
 */
 
-class Solution {
+/*class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
         if(n == 0)
             return;
@@ -52,5 +52,23 @@ class Solution {
     public void copyRest(int[] src, int[] dest, int k, int n) {
         for(int i = k; i < dest.length; i++)
             dest[i] = src[n++];
+    }
+}*/
+
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = m - 1;
+        int j = n - 1;
+        int k = m + n -1;
+        while(i >= 0 && j >= 0) {
+            if(nums1[i] > nums2[j])
+                nums1[k--] = nums1[i--];
+            else
+                nums1[k--] = nums2[j--];
+        }
+        while(i >= 0)
+            nums1[k--] = nums1[i--];
+        while(j >= 0)
+        	nums1[k--] = nums2[j--];
     }
 }
